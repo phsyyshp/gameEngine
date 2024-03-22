36a3049fbec7f787436423312d91978875175fd9
“physsyhp” <theoriginal169@gmail.com>
2024-03-21 20:03:19 -0400

    simple experiment of moving a circle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <SFML/Graphics.hpp>
 int main() {
-  sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+  sf::ContextSettings settings;
+  // settings.antialiasingLevel = 13;
+  sf::RenderWindow window(sf::VideoMode(800, 600), "My window",
+                          sf::Style::Default);
+  window.setFramerateLimit(60);
+  sf::CircleShape shape(50.f);
+  shape.setFillColor(sf::Color(100, 250, 50));
+  float xCoord = 10.f;
+  float yCoord = 50.f;
+
+  shape.setPosition(xCoord, yCoord);
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
@@ -8,7 +18,21 @@ int main() {
         window.close();
       }
     }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+      shape.move(5.f, 0.f);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+      shape.move(-5.f, 0.f);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+      shape.move(0.f, -5.f);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+      shape.move(0.f, 5.f);
+    }
+
     window.clear(sf::Color::Black);
+    window.draw(shape);
     window.display();
   }
   return 0;
