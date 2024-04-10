708853632ba7efefce380fa64a444acc5f6b050b
“physsyhp” <theoriginal169@gmail.com>
2024-04-10 03:45:56 -0400

    Add new utility functions for vector operations

--- core/include/utils.hpp
+++ core/include/utils.hpp
@@ -4,6 +4,8 @@
 #include <iostream>
 float cross(const sf::Vector2f &, const sf::Vector2f &);
 float dot(const sf::Vector2f &, const sf::Vector2f &);
+float absDot(const sf::Vector2f &, const sf::Vector2f &);
+
 float magnitude(const sf::Vector2f &);
 sf::Vector2f normalise(sf::Vector2f);
 sf::Vector2f perpendicular(const sf::Vector2f &);
@@ -13,3 +15,6 @@ sf::Vector2f transformToCordinateSystem(const sf::Vector2f &,
 sf::Vector2f inverseTransformToCordinateSystem(const sf::Vector2f &,
                                                const sf::Vector2f &, float);
 sf::Vector2f rotate(const sf::Vector2f &, float);
+std::array<sf::Vector2f, 2> getBaseCoordinateSystem(float);
+sf::Vector2f elementViseMultipication(const sf::Vector2f &,
+                                      const sf::Vector2f &);
--- core/src/utils.cpp
+++ core/src/utils.cpp
@@ -6,8 +6,16 @@ float cross(const sf::Vector2f &a, const sf::Vector2f &b) {
 float dot(const sf::Vector2f &a, const sf::Vector2f &b) {
   return a.x * b.x + a.y * b.y;
 }
+float absDot(const sf::Vector2f &a, const sf::Vector2f &b) {
+  return std::abs(dot(a, b));
+}
+
 sf::Vector2f perpendicular(const sf::Vector2f &a) { return {-a.y, a.x}; }
 
+sf::Vector2f elementViseMultipication(const sf::Vector2f &vectorA,
+                                      const sf::Vector2f &vectorB) {
+  return {vectorA.x * vectorB.x, vectorA.y * vectorB.y};
+}
 float magnitude(const sf::Vector2f &a) { return std::sqrt(dot(a, a)); }
 sf::Vector2f normalise(sf::Vector2f a) {
   float mag = magnitude(a);
@@ -22,6 +30,15 @@ sf::Vector2f rotate(const sf::Vector2f &vectorToRotate, float angle) {
   rotatedVector.y =
       vectorToRotate.x * std::sin(angle) + vectorToRotate.y * std::cos(angle);
   return rotatedVector;
+}
+std::array<sf::Vector2f, 2> getBaseCoordinateSystem(float orientationOfOrigin) {
+  std::array<sf::Vector2f, 2> baseVector;
+  baseVector[0] = {std::cos(orientationOfOrigin),
+                   std::sin(orientationOfOrigin)};
+  baseVector[1] = {-std::sin(orientationOfOrigin),
+                   std::cos(orientationOfOrigin)};
+
+  return baseVector;
 }
 sf::Vector2f transformToCordinateSystem(const sf::Vector2f &vectorToTransformed,
                                         const sf::Vector2f &positionOfOrigin,
