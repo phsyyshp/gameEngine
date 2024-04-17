8aec26675b9423f5e63ab8f380ea69a317c1e003
“physsyhp” <theoriginal169@gmail.com>
2024-04-16 21:05:55 -0400

    Update collisionHandler.hpp and gjkTests.cpp

--- core/include/collisionHandler.hpp
+++ core/include/collisionHandler.hpp
@@ -2,6 +2,7 @@
 #include "contact.hpp"
 #include "rigidBody2D.hpp"
 #include "shapes.hpp"
+#include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <cmath>
 class ContactManifold {
@@ -49,7 +50,8 @@ public:
   static sf::Vector2f getSupportS(const Circle &circle,
                                   const sf::Vector2f &direction);
 
-  static bool GJKintersectionPP(Box &shapeA, Box &shapeB, CollisionData &cd);
+  static bool GJKintersectionPP(Box &shapeA, Box &shapeB, CollisionData &cd,
+                                sf::RenderWindow *);
   static bool GJKintersectionSP(const Circle &circle,
                                 const std::vector<sf::Vector2f> &verticesB);
   static bool nearestSimplex(std::vector<sf::Vector2f> &simplex,
